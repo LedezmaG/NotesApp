00ea234dec54daf352da68f357d68dc1ed07be57
LedezmaG <israeledezmag@gmail.com>
2020-10-16 20:48:13 -0500

    build

---
+++ docs/precache-manifest.7a1b3562b43ae4d23eb52a8a61df5f9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5415244d841dc87cf97d7d5fd504b8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02510dd6a38657021554",
+    "url": "/static/css/main.c8c1d792.chunk.css"
+  },
+  {
+    "revision": "bd986bb63637a51c004c",
+    "url": "/static/js/2.9511a429.chunk.js"
+  },
+  {
+    "revision": "94efaf9ef8dfdcc9b58d075c8086b235",
+    "url": "/static/js/2.9511a429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02510dd6a38657021554",
+    "url": "/static/js/main.26443e19.chunk.js"
+  },
+  {
+    "revision": "694a932fc6a34b127793",
+    "url": "/static/js/runtime-main.82c598a7.js"
+  }
+]);
